332a77768921549eb5e7ec651f1aa6e84ced02a7
daniel <daniel.okyere@nexum.com>
2021-05-20 11:27:56 +0000

    Created connection to database

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 //Environment Variable
-const env = require('dotenv');
-const bodyParser = require('body-parser');
+const env = require("dotenv");
 env.config();
 
-//Routes
+const bodyParser = require("body-parser");
 
+//Routes
+const loginRoute = require("./src/routes/login");
+
+//Database Connection
+mongoose.connect(
+  `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@evermateslearning.v59pc.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.use(bodyParser());
-app.get('/',(req, res, next)=>{
-    res.json({
-        message: "Welcome to Server"
-    })
+app.get("/", (req, res, next) => {
+  res.json({
+    message: "Welcome to Server",
+  });
 });
-
 
 app.listen(process.env.PORT);
