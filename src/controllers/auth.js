5c934e376b524fce7d0e5ea80c620d8e036783dc
daniel <daniel.okyere@nexum.com>
2021-05-20 13:25:49 +0000

    installed bycrypt for password hashing

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,12 +1,55 @@
-'use strict';
+"use strict";
 
-const User = require('../models/user');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-exports.login = (req,res)=>{
-    User.findOne({email: req.body.email}.exec(async (error, user)=>{
-        if(error) return res.status(400).json({error});
-        if(user){
-            const isPassword = await user.authenticate(req.body.password);
-        };
-    }));
+const generateJwtToken = (_id, role) => {
+  return jwt.sign({ _id, role }, process.env.JWT_SECRET, {
+    expiresIn: "4h",
+  });
+};
+
+exports.signup = (req, res) => {
+  User.findOne({ email: req.body.email }).exec(async (error, user) => {
+    if (user)
+      return res.status(400).json({
+        error: "User already exist",
+      });
+
+    const { fistName, lastName, email, password } = req.body;
+    const hash_password = await bcrypt.hash(password, 10);
+    const _user = new User({
+      firstName,
+      lastName,
+      email,
+      hash_password,
+    });
+    _user.save((error, user) => {
+      if (error) {
+        return res.status(400).json({
+          message: "Something went wrong",
+        });
+      }
+      if (user) {
+        const token = generateJwtToken(user._id, user.role);
+        const { _id, firstName, lastName, email, role, fullName } = user;
+        return res.status(201).json({
+          token,
+          user: { _id, firstName, lastName, email, role, fullName },
+        });
+      }
+    });
+  });
+};
+
+exports.login = (req, res) => {
+  User.findOne(
+    { email: req.body.email }.exec(async (error, user) => {
+      if (error) return res.status(400).json({ error });
+      if (user) {
+        const isPassword = await user.authenticate(req.body.password);
+      }
+    })
+  );
 };
