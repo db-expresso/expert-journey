5630f2bb1ffb4e06d5aa162ad51944d5765d2005
daniel <daniel.okyere@nexum.com>
2021-05-20 12:14:06 +0000

    successful database connection

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const loginRoute = require("./src/routes/login");
 
 //Database Connection
 mongoose.connect(
-  `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@evermateslearning.v59pc.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
+    `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.9qsnv.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
