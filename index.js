776e0e6a36485127c50b0e2b998e3c4e547587fb
daniel <daniel.okyere@nexum.com>
2021-05-21 17:25:15 +0000

    preparing for deployment

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
 
 //Environment Variable
 const env = require("dotenv");
@@ -13,8 +15,8 @@ const { requireSignin } = require("./src/middlewares");
 
 //Routes
 const loginRoute = require("./src/routes/login");
-const profile= require('./src/routes/login');
-const inventoryRoutes= require('./src/routes/inventory');
+const profile = require("./src/routes/login");
+const inventoryRoutes = require("./src/routes/inventory");
 
 //Database Connection
 mongoose
@@ -30,10 +32,13 @@ mongoose
   .then(() => {
     console.log("DB connected");
   });
+
+app.use(compression());
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api", loginRoute);
-app.use('/api',profile);
-app.use('/api',requireSignin,inventoryRoutes);
+app.use("/api", profile);
+app.use("/api", requireSignin, inventoryRoutes);
 
 app.listen(process.env.PORT);
