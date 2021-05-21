f3e0df92c1dce62cea35fce5289b15270a27a3f6
daniel <daniel.okyere@nexum.com>
2021-05-21 11:06:15 +0000

    Create and delete Inventory endpoints successful

--- src/controllers/inventory.js
+++ src/controllers/inventory.js
@@ -1,9 +1,40 @@
-'use strict';
+"use strict";
 
-const inventory = require('../models/inventory');
+const inventory = require("../models/inventory");
 
+exports.createInventory = (req, res) => {
+  const { inventoryName, inventoryId, quantity } = req.body;
 
-exports.createInventory = (req, res) =>{
+  const item = new inventory({
+    inventoryName: inventoryName,
+    inventoryId: inventoryId,
+    quantity: quantity,
+  });
 
+  item.save((error, item) => {
+    if (error) return res.status(400).json({ error });
+    if (item) {
+      res.status(201).json({ item });
+    }
+  });
 };
 
+exports.deleteInventory = async (req, res) => {
+  console.log(req.params);
+  //const { inventoryName, inventoryId, quantity } = req.body;
+  //const id = req.params.id;
+  const item = await inventory.findOne({inventoryId: req.params.inventoryId}).exec();
+  console.log(item);
+  if (!item) {
+    return res.status(400).json({ message: "Inventory not found" });
+  } else {
+    await inventory.findByIdAndDelete(item._id);
+    res.status(200).json({ message: "Inventory deleted" });
+  }
+};
+
+exports.updateInventory = (req, res) => {
+  const { inventoryName, inventoryId, quantity } = req.body;
+  if (item) {
+  }
+};
--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -2,9 +2,12 @@
 
 const express = require("express");
 const router = express.Router();
-const{createInventory}= require('../controllers/inventory');
+const {
+  createInventory,
+  deleteInventory,
+} = require("../controllers/inventory");
 
-
-router.post("/create-inventory",createInventory);
+router.post("/create-inventory", createInventory);
+router.delete("/delete-inventory/:inventoryId", deleteInventory);
 
 module.exports = router;
